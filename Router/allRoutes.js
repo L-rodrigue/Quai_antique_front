7fc3e6b3602cf08ba11be90c4a41f97f6f1561d6
Rodrigue Lepez <lepezrodrigue1@gmail.com>
2024-02-05 13:30:14 +0100

    add modal edit/delete galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/signin", "connexion", "/pages/signin.html"),
-    new Route("/signup", "Nouveau compte", "/pages/signup.html")
+    new Route("/signin", "connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Nouveau compte", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte ", "/pages/auth/account.html"),
+    new Route("/editpassword", "changement mot de passe ", "/pages/auth/editpassword.html"),
 
 ];
     

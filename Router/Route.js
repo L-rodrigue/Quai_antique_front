249b2ebb9734bb75c3ed36109a5c66f75e5a50f3
Rodrigue Lepez <lepezrodrigue1@gmail.com>
2024-02-12 13:07:28 +0100

    modifications router

--- Router/Route.js
+++ Router/Route.js
@@ -1,7 +1,8 @@
-export default class Route { constructor(url, title, pathHtml, pathJS = "") {
+export default class Route { constructor(url, title, pathHtml, autorize ,pathJS = "") {
      this.url = url;
      this.title = title;
     this.pathHtml = pathHtml;
     this.pathJS = pathJS;
+    this.autorize = autorize;
     }
 }
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,14 +2,14 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/signin", "connexion", "/pages/auth/signin.html", "/js/auth/signin.js"),
-    new Route("/signup", "Nouveau compte", "/pages/auth/signup.html", "/js/auth/signup.js"),
-    new Route("/account", "Mon compte ", "/pages/auth/account.html"),
-    new Route("/editpassword", "changement mot de passe ", "/pages/auth/editpassword.html"),
-    new Route("/allResa", "Vos réservation ", "/pages/Réservation/allResa.html"),
-    new Route("/reserver", "Réservation ", "/pages/Réservation/reserver.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/galerie", "La galerie", "/pages/galerie.html", []),
+    new Route("/signin", "connexion", "/pages/auth/signin.html",["disconnected"],  "/js/auth/signin.js"),
+    new Route("/signup", "Nouveau compte", "/pages/auth/signup.html",["disconnected"], "/js/auth/signup.js"),
+    new Route("/account", "Mon compte ", "/pages/auth/account.html",["client", "admin"]),
+    new Route("/editpassword", "changement mot de passe ", "/pages/auth/editpassword.html"["client", "admin"]),
+    new Route("/allResa", "Vos réservation ", "/pages/Réservation/allResa.html",["client"]),
+    new Route("/reserver", "Réservation ", "/pages/Réservation/reserver.html",["client"]),
 
 ];
     
